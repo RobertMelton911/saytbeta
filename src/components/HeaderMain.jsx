c74fa0633c79e3eb496c33696aee274215325ac6
Ama38 <nigmatovm10@gmail.com>
2025-04-26 21:31:46 +0500

    added new fetch to user balance

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./HeaderMain.module.css";
 import { useNavigate } from "react-router-dom";
 
 const HeaderMain = () => {
   const navigate = useNavigate();
-
+  const [demoBalance, setDemoBalance] = useState(1000);
   const handleNavigation = (path) => navigate(path);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("accessToken");
 
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/profile`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        });
+         if (!response.ok) {
+           throw new Error("Could not find token");
+         }
+
+         const data = await response.json();
+         setDemoBalance(data.balance)
+         console.log("Response", data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  })
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -17,7 +40,7 @@ const HeaderMain = () => {
           {/* Секция аккаунта */}
           <div className={styles.account} onClick={() => handleNavigation("/grafpage")} >
             <span className={styles.demoText}>Demo account</span>
-            <b className={styles.balance}>$1,000.00</b>
+            <b className={styles.balance}>${demoBalance}</b>
           </div>
 
           {/* Кнопка депозита */}
